c7b44144c99f6bda2a0ba1dc1a30841874fa419e
Marta Guimafordev <marta.masson@guimafordev.onmicrosoft.com>
2023-02-02 11:37:59 -0300

    Ajuste versao functions

--- EventHub/terraform/azure resources.tf
+++ EventHub/terraform/azure resources.tf
@@ -92,7 +92,7 @@ resource "azurerm_windows_function_app" "labsEventHubFunctionApp" {
 
   site_config {
       application_stack {
-      dotnet_version = "3.1"
+      dotnet_version = "4.0"
     }
   }
 }
